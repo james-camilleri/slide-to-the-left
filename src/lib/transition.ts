218f6a3a357a7f096df1a66913705cf4e1015bfd
James Camilleri <james@james.mt>
2024-10-14 10:52:05 +0100

    Update transition types

--- src/lib/transition.ts
+++ src/lib/transition.ts
@@ -1,15 +1,17 @@
 import { quadOut } from 'svelte/easing'
-import { type TransitionConfig, fly } from 'svelte/transition'
+import { type TransitionConfig as SvelteTransitionConfig, fly } from 'svelte/transition'
 
-type TransitionFunction = (
+export type TransitionFunction = (
   node: Element,
   params: never,
   options?: { direction: 'in' | 'out' | 'both' },
-) => TransitionConfig
-type SlideTransitions = { in: TransitionFunction; out: TransitionFunction }
+) => SvelteTransitionConfig
 
-export const transition = (direction: number) =>
-  ({
-    in: (node) => fly(node, { x: `${100 * direction}%`, duration: 500, easing: quadOut }),
-    out: (node) => fly(node, { x: `${100 * direction * -1}%`, duration: 500, easing: quadOut }),
-  }) satisfies SlideTransitions
+export type SlideTransitionsConfig = { in: TransitionFunction; out: TransitionFunction }
+export type TransitionConfigFunction = (direction: number) => SlideTransitionsConfig
+
+// Add your transitions here.
+export const transition: TransitionConfigFunction = (direction) => ({
+  in: (node) => fly(node, { x: `${100 * direction}%`, duration: 500, easing: quadOut }),
+  out: (node) => fly(node, { x: `${100 * direction * -1}%`, duration: 500, easing: quadOut }),
+})
